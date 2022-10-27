2539425723edd6d57dbc2ad6db447f3f8dad799b
Kris Hamade <kris.hamade@hamy.dev>
2022-10-26 23:28:30 -0400

    Basic Test App

---
+++ CommunityInformation/CommunityInformation.lua
@@ -0,0 +1,4 @@
+SLASH_COMMUNITYINFORMATION1 = "/ci";
+SlashCmdList["COMMUNITYINFORMATION"] = function(msg)
+    print("Hello, WoW!")
+end
---
+++ copy.bat
@@ -0,0 +1,2 @@
+XCOPY '.\CommunityInformation\' 'C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\CommunityInformation\' /e /d /i
+pause
