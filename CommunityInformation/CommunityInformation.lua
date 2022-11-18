18a8a286793c997e9d0145ab9c30a12370e5a7bd
Kris Hamade <kris.hamade@hamy.dev>
2022-11-17 21:25:39 -0500

    Working Addon
    Successfully Exports Community Data

--- CommunityInformation/CommunityInformation.lua
+++ CommunityInformation/CommunityInformation.lua
@@ -1,20 +1,27 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, ...)
-    local numberOfMembers, count, memberInfo, i;
+    local numberOfMembers, count, i;
     memberArray = {};
-    allCommunityMembers = allCommunityMembers or {};
+    allCommunityMembers = {};
+    allCommunityMembersData = {};
     i = 0;
     totalNumberOfMembers = 0;
     clubId = 396624561;
     
     info = C_Club.GetClubInfo(clubId);
     members = C_Club.GetClubMembers(clubId);
+    
     for key,value in pairs(members) do 
-       totalNumberOfMembers = totalNumberOfMembers + 1 
-       for key,id in pairs(C_Club.GetMemberInfo(clubId, value)) do
-          allCommunityMembers[i] = id
-          i = i + 1 
-       end
+       memberArray[key] = value
+    end
+    
+    for key,value in pairs(memberArray) do
+       allCommunityMembers[i] = C_Club.GetMemberInfo(clubId, value)
+       i = i + 1 
+    end
+    
+    for key,value in pairs(allCommunityMembers) do
+       print(key,value)
     end
 end)
