116019ab5243933c30d86b53c28d143344e21fa0
Kris Hamade <kris.hamade@hamy.dev>
2022-11-17 21:20:40 -0500

    Partially Working

--- CommunityInformation/CommunityInformation.lua
+++ CommunityInformation/CommunityInformation.lua
@@ -1,4 +1,20 @@
-SLASH_COMMUNITYINFORMATION1 = "/ci";
-SlashCmdList["COMMUNITYINFORMATION"] = function(msg)
-    print("Hello, WoW!")
-end
+local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, ...)
+    local numberOfMembers, count, memberInfo, i;
+    memberArray = {};
+    allCommunityMembers = allCommunityMembers or {};
+    i = 0;
+    totalNumberOfMembers = 0;
+    clubId = 396624561;
+    
+    info = C_Club.GetClubInfo(clubId);
+    members = C_Club.GetClubMembers(clubId);
+    for key,value in pairs(members) do 
+       totalNumberOfMembers = totalNumberOfMembers + 1 
+       for key,id in pairs(C_Club.GetMemberInfo(clubId, value)) do
+          allCommunityMembers[i] = id
+          i = i + 1 
+       end
+    end
+end)
